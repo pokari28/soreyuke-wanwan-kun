6ec529c29fba0f9fc33d8e50e5c9055cff58ebd1
pokari28 <yuzix02@gmail.com>
2026-09-09 16:47:35 +0900

    Do not overwrite complete images

--- img-assemble.js
+++ img-assemble.js
@@ -2,6 +2,6 @@ window.IMAGE_DATA=window.IMAGE_DATA||{};
 (function(){
   var d=IMAGE_DATA,c=window._IC||{};
   ["beginnerIdle","beginnerGood","beginnerBad","proIdle","proIdlePlus","proBad","milestone10","milestone20","milestone30","endingPerfect"].forEach(function(k){
-    if(c[k+"a"]&&c[k+"b"]) d[k]="data:image/jpeg;base64,"+c[k+"a"]+c[k+"b"];
+    if(!d[k] && c[k+"a"] && c[k+"b"]) d[k]="data:image/jpeg;base64,"+c[k+"a"]+c[k+"b"];
   });
 })();
